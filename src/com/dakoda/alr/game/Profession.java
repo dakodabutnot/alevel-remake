5ed040a3f954d68f88a95db30ea56905d7505931
dakodabutnot <southforwinter6@gmail.com>
2018-09-01 17:09:29 +0100

    Defined new set of professions. Four for each armour type.

--- src/com/dakoda/alr/game/Profession.java
+++ src/com/dakoda/alr/game/Profession.java
@@ -2,9 +2,24 @@ package com.dakoda.alr.game;
 
 public enum Profession {
 
-    WARRIOR("Warrior"),
-    RANGER("Ranger"),
-    MAGE("Mage");
+    DUELIST("Duelist"), // equivalent of a Warrior
+    PALADIN("Paladin"), // self-explanatory
+    ANTIQUAN("Antiquan"), // similar to Revenant from Guild Wars 2
+                          // channels a legend or an emotion to the extreme
+    PERFORATOR("Perforator"), // similar to Lancer in other MMOs
+
+    ARCHER("Archer"), // self-explanatory
+    ASSASSIN("Assassin"), //self-explanatory
+    NAGUAL("Nagual"), // shape-shifter
+    HUNTER("Hunter"), // similar to Archer, but specialises guns and tools
+                      // think Van Helsing
+
+    SHAMAN("Shaman"), // equivalent of a Mage/Wizard/Witch (with some abilities of Necromancer)
+    PRIEST("Priest"), // healer
+    CHRONOL("Chronol"), // specialises in time-bending abilities
+                        // abilities that take time to do,
+                        // reverting conditions, etc.
+    BARD("Bard"); // self-explanatory
 
     private String displayText;
 
