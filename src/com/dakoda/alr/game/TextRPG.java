53cae1e62eda58527bb9f3c4c9cd6d2c560ec3f0
jpatric <southforwinter6@gmail.com>
2018-09-05 20:57:07 +0100

    new Main

---
+++ src/com/dakoda/alr/game/TextRPG.java
@@ -0,0 +1,9 @@
+package com.dakoda.alr.game;
+import com.dakoda.alr.game.quest.Quest;
+
+public class TextRPG {
+
+    public static void main(String[] args) {
+        Quest quest = new Quest.QuestBuilder().build();
+    }
+}
