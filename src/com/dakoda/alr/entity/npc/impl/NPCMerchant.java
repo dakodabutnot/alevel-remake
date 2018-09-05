61d0fae8ec65abd70dc6f6286273fbe89f885880
jpatric <southforwinter6@gmail.com>
2018-09-05 20:56:02 +0100

    Changed NPC to abstract class.

--- src/com/dakoda/alr/entity/npc/impl/NPCMerchant.java
+++ src/com/dakoda/alr/entity/npc/impl/NPCMerchant.java
@@ -2,5 +2,5 @@ package com.dakoda.alr.entity.npc.impl;
 import com.dakoda.alr.entity.npc.NPC;
 import com.dakoda.alr.entity.npc.Merchant;
 
-public class NPCMerchant implements NPC, Merchant {
+public class NPCMerchant extends NPC implements Merchant {
 }
