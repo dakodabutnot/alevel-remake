b349a2e512421afd373accbc8f01c0a4d7de7203
dakodabutnot <southforwinter6@gmail.com>
2018-09-06 15:00:00 +0100

    enum to define base stats of each profession

---
+++ src/com/dakoda/alr/game/profession/ProfessionBaseStats.java
@@ -0,0 +1,20 @@
+package com.dakoda.alr.game.profession;
+
+public enum ProfessionBaseStats {
+
+    DUELIST,
+    PALADIN,
+    ANTIQUER,
+    PERFORATOR,
+
+    ARCHER,
+    ASSASSIN,
+    NAGUAL,
+    HUNTER,
+
+    SHAMAN,
+    PRIEST,
+    CHRONOL,
+    BARD;
+
+}
