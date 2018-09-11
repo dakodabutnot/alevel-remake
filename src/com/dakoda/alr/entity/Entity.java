8e4307d5f48b0b5db67e3ded448e89bb1c0623dd
dakodabutnot <southforwinter6@gmail.com>
2018-09-11 14:13:43 +0100

    standard Entity fields

--- src/com/dakoda/alr/entity/Entity.java
+++ src/com/dakoda/alr/entity/Entity.java
@@ -1,10 +1,19 @@
 package com.dakoda.alr.entity;
+import com.dakoda.alr.entity.dna.DNA;
+import com.dakoda.alr.game.character.Profession;
 
 public abstract class Entity {
 
+    private String name;
     private Attributes attributes;
+    private DNA dna;
+    private Profession profession;
 
     public Attributes getAttributes() {
         return attributes;
+    }
+
+    public DNA getDNA() {
+        return dna;
     }
 }
