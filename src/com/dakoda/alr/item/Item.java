d47944963e6822e54e63f450611b44e720800bc5
dakodabutnot <southforwinter6@gmail.com>
2018-09-11 15:39:51 +0100

    moved ItemType into Item class

--- src/com/dakoda/alr/item/Item.java
+++ src/com/dakoda/alr/item/Item.java
@@ -3,14 +3,14 @@ package com.dakoda.alr.item;
 public abstract class Item {
 
     private String name;
-    private ItemType itemType;
+    private Item.Type itemType;
     private int moneyValue;
 
     public String getName() {
         return name;
     }
 
-    public ItemType getItemType() {
+    public Item.Type getItemType() {
         return itemType;
     }
 
@@ -32,12 +32,35 @@ public abstract class Item {
         this.moneyValue = moneyValue;
     }
 
-    public void setItemType(ItemType itemType) {
+    public void setItemType(Item.Type itemType) {
         this.itemType = itemType;
     }
 
     @Override
     public boolean equals(Object obj) {
         return (obj instanceof Item) && ((Item) obj).getItemType().equals(this.itemType) && (((Item) obj).getName().equals(this.name));
+    }
+
+    public enum Type {
+
+        JUNK("Junk"),
+        CRAFTING_MATERIAL("Material"),
+        CONSUMABLE("Consumable"),
+        WEAPON("Weapon"),
+        ARMOUR("Armour"),
+        TREASURE("Treasure"),
+        QUEST("Quest-specific item");
+
+        private String displayText;
+
+        Type(
+                String displayText
+        ) {
+            this.displayText = displayText;
+        }
+
+        public String getDisplayText() {
+            return displayText;
+        }
     }
 }
