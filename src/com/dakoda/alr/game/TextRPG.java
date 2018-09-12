1b3d7951b254da8ec37ed9f7c45865536e53bdd8
dakodabutnot <southforwinter6@gmail.com>
2018-09-12 11:09:01 +0100

    inventory testing

--- src/com/dakoda/alr/game/TextRPG.java
+++ src/com/dakoda/alr/game/TextRPG.java
@@ -2,10 +2,8 @@ package com.dakoda.alr.game;
 import com.dakoda.alr.entity.player.Inventory;
 import com.dakoda.alr.game.quest.Quest;
 import com.dakoda.alr.item.Item;
-import com.dakoda.alr.item.ItemType;
+import static com.dakoda.alr.item.Item.Type.*;
 import com.dakoda.alr.item.Items;
-import java.util.Map.Entry;
-
 public class TextRPG {
 
     //stop committing things irrelevant to the branch you're better than this
@@ -15,16 +13,27 @@ public class TextRPG {
                 .addItemRequirement(Items.Quest.APPLE.get(), 10)
                 .build();
 
+        Inventory inventory = builtInventory();
+
+        inventory.getAllItems().forEach((key, value) -> System.out.println(key.getName() + " | " + key.getItemType().getDisplayText() + " | " + value.toString()));
+
+//        inventory.getFiltered(WEAPON)
+//                .forEach((key, value) -> System.out.println(key.getName() + " | " + key.getItemType().getDisplayText() + " | " + value.toString()));
+    }
+
+    static Inventory builtInventory() {
         Inventory inventory = new Inventory();
-        inventory.addItem(Items.Consumable.APPLE.get());
-        inventory.addItem(Items.Consumable.APPLE.get(), 2);
-        inventory.addItem(Items.Consumable.APPLE.get(), -1);
-        inventory.addItem(Items.Consumable.APPLE.get(), 3);
 
+        inventory.addItem(Items.Consumable.APPLE.get(), 4);
+        inventory.addItem(Items.Consumable.APPLE.get(), 1);
         inventory.addItem(Items.Quest.APPLE.get());
+        inventory.addItem(Items.Armour.IRON_HELMET.get());
+        inventory.addItem(Items.Weapon.BOW.get());
+        inventory.addItem(Items.Weapon.IRON_SWORD.get());
+        inventory.addItem(Items.Treasure.HAND_OF_MIDAS.get());
+        inventory.addItem(Items.Junk.TRASH.get(), 10);
+        inventory.addItem(Items.Material.WOOD.get(), 40);
 
-        for (Entry<Item, Integer> entry : inventory.getFiltered(ItemType.CONSUMABLE).entrySet()) {
-            System.out.println(entry.getKey().getName() + " | " + entry.getKey().getItemType().getDisplayText() + " | " + entry.getValue().toString());
-        }
+        return inventory;
     }
 }
