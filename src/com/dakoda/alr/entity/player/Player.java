590b36d190a66f97d9ede652c1f53d98a4ce6fec
dakodabutnot <southforwinter6@gmail.com>
2018-09-13 09:35:50 +0100

    field and method rename

--- src/com/dakoda/alr/entity/player/Player.java
+++ src/com/dakoda/alr/entity/player/Player.java
@@ -3,13 +3,13 @@ import com.dakoda.alr.entity.Entity;
 
 public final class Player extends Entity {
 
-    private Inventory inventory;
+    private Inventory inv;
 
     public Player() {
 
     }
 
-    public Inventory getInventory() {
-        return inventory;
+    public Inventory inventory() {
+        return inv;
     }
 }
