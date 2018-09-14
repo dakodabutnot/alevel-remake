45ea9b27cf112e420d098bb7aaff12fc12211b05
dakodabutnot <southforwinter6@gmail.com>
2018-09-14 20:10:27 +0100

    Javadoc

--- src/com/dakoda/alr/game/player/Inventory.java
+++ src/com/dakoda/alr/game/player/Inventory.java
@@ -9,6 +9,12 @@ public final class Inventory {
     private HashMap<Item, Integer> items = new HashMap<>();
     private long currency;
 
+    /**
+     * Increases the inventory's currency value by the given value.
+     * A negative value parsed here will validate as if it was positive.
+     *
+     * @param add The amount to be added.
+     */
     public void addCurrency(long add) {
         if (add > 0) {
             if (checkValidCurrencyChange(add)) currency += add;
@@ -17,6 +23,12 @@ public final class Inventory {
         }
     }
 
+    /**
+     * Decreases the inventory's currency value by the given value.
+     * A negative value parsed here will validate as if it was positive.
+     *
+     * @param sub The amount to be subtracted.
+     */
     public void subCurrency(long sub) {
         if (sub > 0) {
             if (checkValidCurrencyChange(-sub)) currency -= sub;
@@ -25,6 +37,17 @@ public final class Inventory {
         }
     }
 
+    /**
+     * Checks to see if a currency value change can be made, taking into consideration
+     * the current value of currency.
+     *
+     * A change cannot be made if the change were to cause the currency value to fall below zero,
+     * and a change cannot be made if the change were to cause the currency value to rise above the maximum
+     * stored value of a long.
+     *
+     * @param change The change to be made.
+     * @return Boolean determining whether or not the change can be made.
+     */
     public boolean checkValidCurrencyChange(long change) {
         return currency + change > 0 && currency + change < Long.MAX_VALUE;
     }
@@ -33,6 +56,17 @@ public final class Inventory {
         addItem(item, 1);
     }
 
+    /**
+     * Adds an item to the inventory, or multiple depending on the value of amount.
+     *
+     * A negative amount here will throw an exception.
+     * An amount of zero here will throw an exception.
+     *
+     * If the item already exists in the Inventory, the items will stack onto that.
+     *
+     * @param item The item to be added.
+     * @param amount The amount of the item to be added.
+     */
     public void addItem(Item item, int amount) {
         if (amount < 0) {
             throw new RuntimeException("You're trying to add a NEGATIVE amount of items to an inventory. Try 'removeItem' instead.");
@@ -47,14 +81,35 @@ public final class Inventory {
         }
     }
 
+    /**
+     * Removes an item from the inventory.
+     *
+     * @param item The item to be removed.
+     */
     public void removeItem(Item item) {
         removeItem(item, 1);
     }
 
+    /**
+     * Removes all of one item from the inventory.
+     *
+     * @param item The item to have all instances of it in the inventory removed.
+     */
     public void removeAllOfItem(Item item) {
         removeItem(item, items.get(item));
     }
 
+    /**
+     * Removes an item from the inventory.
+     *
+     * A negative amount here will throw an exception.
+     * An amount of zero here will throw an exception.
+     *
+     * If the item doesn't exist, an exception will be thrown.
+     *
+     * @param item The item to be removed.
+     * @param amount
+     */
     public void removeItem(Item item, int amount) {
         if (amount < 0) {
             throw new RuntimeException("You're trying to remove a NEGATIVE amount of items from an inventory. Try 'addItem' instead.");
@@ -70,6 +125,10 @@ public final class Inventory {
         removeRedundantEntries();
     }
 
+    /**
+     * Removes all items from the inventory that have an amount that is either negative or zero.
+     * Logs the amount of redundant entries removed to the console.
+     */
     public void removeRedundantEntries() {
         int removed = 0;
         for (Map.Entry<Item, Integer> entry : items.entrySet()) {
@@ -87,6 +146,15 @@ public final class Inventory {
         return items;
     }
 
+    /**
+     * Filters items in the inventory by Item.Type
+     *
+     * Uses a stream to filter items that match the given Item.Type and collects them
+     * in a new HashMap.
+     *
+     * @param itemType The Item.Type we are filtering for.
+     * @return HashMap of filtered items.
+     */
     public HashMap<Item, Integer> getFiltered(Item.Type itemType) {
         return (HashMap<Item, Integer>)
                 (getAllItems().entrySet().stream()
