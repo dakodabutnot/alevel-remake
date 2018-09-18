349b1b014177edc2c5f2268a181b79c94cb0b0e9
dakodabutnot <southforwinter6@gmail.com>
2018-09-18 09:54:16 +0100

    test for Currency for all units, flexible and is able to validate correctly if a new currency is added,

--- src/test/java/com/dakoda/alr/game/util/CurrencyTest.java
+++ src/test/java/com/dakoda/alr/game/util/CurrencyTest.java
@@ -15,4 +15,24 @@ public class CurrencyTest {
                     Currency.convertToUnits(unit.value()));
         }
     }
+
+    @Test
+    public void test_convertToUnits_allUnits() {
+        StringBuilder result = new StringBuilder();
+        for (Currency.Unit unit : Currency.Unit.values()) {
+            if (unit.ordinal() != 0) {
+                result.insert(0, ", ");
+            }
+            result.insert(0, unit.name().toLowerCase()).insert(0, "1 ");
+        }
+        int compare = 0;
+        for (Currency.Unit unit : Currency.Unit.values()) {
+            compare += unit.value();
+        }
+        Assert.assertEquals(
+                result.toString(),
+                Currency.convertToUnits(compare)
+        );
+    }
 }
+
