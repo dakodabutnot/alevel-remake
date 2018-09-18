a92e5672c213a573b90447761ec7553ce04e6bf4
dakodabutnot <southforwinter6@gmail.com>
2018-09-18 11:30:02 +0100

    unit tests

---
+++ src/test/java/com/dakoda/alr/game/battle/LootTest.java
@@ -0,0 +1,23 @@
+package com.dakoda.alr.game.battle;
+import com.dakoda.alr.game.world.item.Item;
+import com.dakoda.alr.game.world.item.Items;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class LootTest {
+
+    @Test
+    public void test_builder() {
+        Item testingItem = Items.Consumable.APPLE.get();
+        Integer testingInteger = 2;
+        Loot loot = new Loot.Builder()
+                .drop(testingItem, testingInteger)
+                .build();
+        Assert.assertNotNull(loot);
+        Assert.assertTrue(loot.getDrops().containsKey(testingItem));
+        Assert.assertTrue(loot.getDrops().containsValue(testingInteger));
+    }
+}
